5887988a1eceeddb2effdd6c764e895cbf0e7744
Konstantin <crs.costya.slepchenk@hubinternational.com>
2024-04-08 13:42:08 +0200

    Added missing return annotations

--- api/auxiliary.py
+++ api/auxiliary.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 @router.post("/insert", response_model=ToDoItem, status_code=201)
-async def create_todo(request: Request, todo_item: ToDoItem):
+async def create_todo(request: Request, todo_item: ToDoItem) -> ToDoItem:
     await db.test_coll.replace_one({"id": todo_item.id}, todo_item.__dict__, True)
     return todo_item
 
@@ -22,14 +22,14 @@ async def create_todo(request: Request, todo_item: ToDoItem):
     response_description="List of all To-do items",
     response_model=List[ToDoItem],
 )
-async def list_todos(request: Request):
+async def list_todos(request: Request) -> List[ToDoItem]:
     cursor = [doc async for doc in db.test_coll.find()]
     logger.info(f"Got list of all To-Do items with len: {len(cursor)}")
     return cursor
 
 
 @router.put("/update", response_model=ToDoItem, status_code=201)
-async def replace_todo(request: Request, item_with_update: ToDoItem):
+async def replace_todo(request: Request, item_with_update: ToDoItem) -> ToDoItem:
     """
     Update an item. Id (which is also the PartitionKey in this case) values should reference the item to be updated:
 
